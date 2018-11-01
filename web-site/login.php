bab3ba18ff6ba01e2c90a48c1c5b91879d6a567e
Federico Marotta <federicomarotta@mail.com>
2018-11-01 16:51:52 +0100

    login.php: fix behaviour when password is wrong

--- web-site/login.php
+++ web-site/login.php
@@ -9,6 +9,8 @@
 		<?php
 		if (!isset($_SERVER["HTTP_REFERER"]))
 			echo "<h3>You have to log in first. Note: you will be redirected to your home page.</h3>";
+		elseif ($_SERVER["HTTP_REFERER"] == "https://git.fmarotta.dynu.net/web-site/login.php")
+			echo "<h3>The password you typed is wrong; try again.</h3>";
 		?>
         <form method="POST" action="/login_handler.php">
             Username: <input type="text" name="httpd_username" /><br>
